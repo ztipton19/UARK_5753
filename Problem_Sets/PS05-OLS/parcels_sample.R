5508de2a318087e8f4bec4c9e3be871a33f4563e
Kyle Butts <buttskyle96@gmail.com>
2026-03-12 10:58:31 -0500

    PS05

--- Problem_Sets/PS05-OLS/parcels_sample.R
+++ Problem_Sets/PS05-OLS/parcels_sample.R
@@ -2,11 +2,12 @@ library(tidyverse)
 library(arrow)
 library(here)
 library(fs)
-fs::dir_create(here("04-Regression_in_Practice/R-day/data/"))
+library(sf)
+fs::dir_create(here("Problem_Sets/PS05-OLS/data/"))
 
 set.seed(20250223)
 parcels <- open_dataset(
-  "~/Dropbox/Zoning-and-Housing-Supply/data/base/MA-parcels_panel_geocoded.parquet"
+  "~/Library/CloudStorage/Dropbox/Projects/Zoning-and-Housing-Supply/data/base/parcels/MA-parcels_panel_geocoded.parquet"
 )
 
 is_whole_number <- \(x) {
@@ -25,11 +26,11 @@ subset <- parcels |>
   filter(building_value >= 1) |>
   filter(land_value >= 1) |>
   filter(between(use_code, 101, 110)) |>
-  filter(between(lot_size_acres, 0.05, 1.5)) |>
+  filter(between(lot_size_acres_from_shape, 0.05, 1.5)) |>
   filter(between(building_area, 1, 10000)) |>
   filter(between(z_mls_acres, 0.01, 2)) |>
   filter(between(n_rooms, 3, 10), is_whole_number(n_rooms)) |>
-  filter(n_stories <= 3, is_whole_number(n_stories)) |>
+  filter(n_stories <= 2, is_whole_number(n_stories)) |>
   select(-ends_with("per_acre")) |>
   collect()
 
@@ -45,11 +46,13 @@ subset <- parcels |>
 
 sample <- subset |>
   select(
+    longitude,
+    latitude,
     fy,
     town,
     census_tract,
     use_code,
-    lot_size_acres,
+    lot_size_acres = lot_size_acres_from_shape,
     building_value,
     land_value,
     other_value,
@@ -81,7 +84,39 @@ sample <- subset |>
   ) |>
   collect()
 
+sample <- sample |>
+  st_as_sf(
+    coords = c("longitude", "latitude"),
+    crs = st_crs(4326),
+    remove = FALSE
+  )
+
+# CBD Definition from Matt Holihan
+# https://mattholian.blogspot.com/2013/05/central-business-district-geocodes.html
+cbd <- data.frame(latitude = 42.361145, longitude = -71.057083) |>
+  st_as_sf(coords = c("longitude", "latitude"), crs = st_crs(4326))
+
+sample$dist_to_cbd_km <-
+  st_distance(
+    sample$geometry,
+    cbd$geometry
+  )[, 1] |>
+  units::set_units("km") |>
+  units::drop_units()
+
+sample <- sample |>
+  mutate(
+    dist_to_cbd_rings = case_when(
+  dist_to_cbd_km <= 5 ~ "[0, 5]",
+  dist_to_cbd_km <= 10 ~ "(5, 10]",
+  dist_to_cbd_km <= 20 ~ "(10, 20]",
+  dist_to_cbd_km <= 30 ~ "(20, 30]",
+  TRUE ~ "30+"
+)) 
+
+sample <- sample |> st_drop_geometry() |> as_tibble()
+
 write_parquet(
   sample,
-  here("04-Regression_in_Practice/R-day/data/MA_parcels_sample.parquet")
+  here("Problem_Sets/PS05-OLS/data/MA_parcels_sample.parquet")
 )
